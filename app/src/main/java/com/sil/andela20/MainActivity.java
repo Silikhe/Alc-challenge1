f35bbdaae93479593787f7e7ce97181fd321126e
Sil silas <silaswans@gmail.com>
2020-09-15 14:39:14 +0300

    first commit

--- app/src/main/java/com/sil/andela20/MainActivity.java
+++ app/src/main/java/com/sil/andela20/MainActivity.java
@@ -3,12 +3,17 @@ package com.sil.andela20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_text = (TextView) findViewById(R.id.tv_dummy);
+
     }
 }
