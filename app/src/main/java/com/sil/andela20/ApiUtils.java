33fb3e71898bc0f1ebc1b8cb8679208b48b4c4a8
Sil silas <silaswans@gmail.com>
2020-09-17 03:06:24 +0300

    test

--- app/src/main/java/com/sil/andela20/ApiUtils.java
+++ app/src/main/java/com/sil/andela20/ApiUtils.java
@@ -1,46 +1,46 @@
-package com.sil.andela20;
-
-import android.util.Log;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
-
-public class ApiUtils {
-    private  ApiUtils () {}
-
-    public static final String BASE_API_URL = "";
-    public static final String QUERY_KEY = "q+";
-
-    public static String buildUrl (String title){
-        String complete = BASE_API_URL + QUERY_KEY + title;
-        return complete;
-    }
-
-    public static String getJson (URL url) throws IOException {
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-
-        try {
-            InputStream stream = connection.getInputStream();
-
-            Scanner scanner = new Scanner(stream);
-            scanner.useDelimiter("\\A");
-
-            boolean hasData = scanner.hasNext();
-            if (hasData){
-                return scanner.next();
-            }else{
-                return null;
-            }
-        }catch (Exception e){
-            Log.d("Error", e.toString());
-            return null;
-        }
-        finally {
-            connection.disconnect();
-        }
-    }
-
-}
+//package com.sil.andela20;
+//
+//import android.util.Log;
+//
+//import java.io.IOException;
+//import java.io.InputStream;
+//import java.net.HttpURLConnection;
+//import java.net.URL;
+//import java.util.Scanner;
+//
+//public class ApiUtils {
+//    private  ApiUtils () {}
+//
+//    public static final String BASE_API_URL = "";
+//    public static final String QUERY_KEY = "q+";
+//
+//    public static String buildUrl (String title){
+//        String complete = BASE_API_URL + QUERY_KEY + title;
+//        return complete;
+//    }
+//
+//    public static String getJson (URL url) throws IOException {
+//        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+//
+//        try {
+//            InputStream stream = connection.getInputStream();
+//
+//            Scanner scanner = new Scanner(stream);
+//            scanner.useDelimiter("\\A");
+//
+//            boolean hasData = scanner.hasNext();
+//            if (hasData){
+//                return scanner.next();
+//            }else{
+//                return null;
+//            }
+//        }catch (Exception e){
+//            Log.d("Error", e.toString());
+//            return null;
+//        }
+//        finally {
+//            connection.disconnect();
+//        }
+//    }
+//
+//}
